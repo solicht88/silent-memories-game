114b2e81865f00e7f647a20f38ef1f532971463a
solicht88 <sakanaasai@gmail.com>
2024-07-20 15:51:50 -0400

    Fixed walking and implemented y-sort for daycare
    - Fixed diagonal walking
    - Fixed animation from playing while not moving
    - Added y-sort for first section of first map

--- scripts/animations/char_1.tscn.gd
+++ scripts/animations/char_1.tscn.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
-@export var speed = 200
+@export var speed = 300
 
 @onready var _animation_player = $AnimationPlayer
 
 func get_input():
 	var input_direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+	if Input.is_action_pressed("walk_right") || Input.is_action_pressed("walk_left"):
+		input_direction.y = 0
+	elif Input.is_action_pressed("walk_up") || Input.is_action_pressed("walk_down"):
+		input_direction.x = 0
 	velocity = input_direction * speed
 
 func _physics_process(_delta):
@@ -13,7 +17,11 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func _process(_delta):
-	if Input.is_action_pressed("walk_left"):
+	if Input.is_action_pressed("walk_right") && Input.is_action_pressed("walk_left"):
+		_animation_player.stop()
+	elif Input.is_action_pressed("walk_up") && Input.is_action_pressed("walk_down"):
+		_animation_player.stop()
+	elif Input.is_action_pressed("walk_left"):
 		_animation_player.play("walk_left")
 	elif Input.is_action_pressed("walk_right"):
 		_animation_player.play("walk_right")
