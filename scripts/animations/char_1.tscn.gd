b72df7457f8d5ee8fc7d9daecfff055dc2e8921e
solicht88 <sakanaasai@gmail.com>
2024-07-20 14:48:10 -0400

    Connect animations to player input, resize assets
    - Connected walk animations to player movement
    - Resized assets to fit map
    - Created tilesets for one section of the map

--- scripts/animations/char_1.tscn.gd
+++ scripts/animations/char_1.tscn.gd
@@ -1,11 +1,25 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 200
+
+@onready var _animation_player = $AnimationPlayer
 
 func get_input():
 	var input_direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+
+func _process(_delta):
+	if Input.is_action_pressed("walk_left"):
+		_animation_player.play("walk_left")
+	elif Input.is_action_pressed("walk_right"):
+		_animation_player.play("walk_right")
+	elif Input.is_action_pressed("walk_up"):
+		_animation_player.play("walk_up")
+	elif Input.is_action_pressed("walk_down"):
+		_animation_player.play("walk_down")
+	else:
+		_animation_player.stop()
