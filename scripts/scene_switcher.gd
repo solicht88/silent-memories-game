1dd29b195f419a7e24d4bea730c90d63f5f967ae
solicht88 <sakanaasai@gmail.com>
2024-07-22 10:58:36 -0400

    Set up scene scene_switcher
    - Created scene_switcher
    - Can switch from reception to playroom at given coords
    - Started to set up y-sort for playroom

---
+++ scripts/scene_switcher.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var current_scene = null
+
+func _ready():
+	var root = get_tree().root
+	current_scene = root.get_child(root.get_child_count() - 1)
+	# print_debug(current_scene)
+
+
+func switch_scene(res_path, x, y):
+	call_deferred("_deferred_switch_scene", res_path, x, y)
+
+
+func _deferred_switch_scene(res_path, x, y):
+	current_scene.free()
+	var s = load(res_path)
+	current_scene = s.instantiate()
+	get_tree().root.add_child(current_scene)
+	get_tree().current_scene = current_scene
+	
+	current_scene.get_node("player").position = Vector2(x, y)
---
+++ scripts/scenes/daycare/reception.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _process(_delta):
+	if Input.is_action_pressed("interact"):
+		SceneSwitcher.switch_scene("res://scenes/maps/daycare/playroom.tscn", 640, 630)
