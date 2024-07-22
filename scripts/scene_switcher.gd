43985f349fd07c64d379509d2a17537ec3d7064a
solicht88 <sakanaasai@gmail.com>
2024-07-22 15:35:34 -0400

    Fixed y-sort not working for playroom.tscn

--- scripts/scene_switcher.gd
+++ scripts/scene_switcher.gd
@@ -5,18 +5,18 @@ var current_scene = null
 func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
-	# print_debug(current_scene)
+	print_debug(current_scene)
 
 
-func switch_scene(res_path, x, y):
-	call_deferred("_deferred_switch_scene", res_path, x, y)
+func switch_scene(res_path):
+	call_deferred("_deferred_switch_scene", res_path)
 
 
-func _deferred_switch_scene(res_path, x, y):
+func _deferred_switch_scene(res_path):
 	current_scene.free()
 	var s = load(res_path)
 	current_scene = s.instantiate()
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
 	
-	current_scene.get_node("player").position = Vector2(x, y)
+	# current_scene.get_node("player").position = Vector2(x, y)
--- scripts/scenes/daycare/reception.gd
+++ scripts/scenes/daycare/reception.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _process(_delta):
 	if Input.is_action_pressed("interact"):
-		SceneSwitcher.switch_scene("res://scenes/maps/daycare/playroom.tscn", 640, 630)
+		SceneSwitcher.switch_scene("res://scenes/maps/daycare/playroom.tscn")
