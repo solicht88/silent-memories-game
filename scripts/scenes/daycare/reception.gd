82b44295f68658e244db99a9f0980986c72a9216
solicht88 <sakanaasai@gmail.com>
2024-07-23 17:19:08 -0400

    finished setting up nap room, added dialogic
    - Adjusted object collisions for nap room
    - Added border collisions for nap room
    - Installed and learning how to use dialogic

--- scripts/scenes/daycare/reception.gd
+++ scripts/scenes/daycare/reception.gd
@@ -1,6 +1,4 @@
 extends Node2D
-'''
-func _process(_delta):
-	if Input.is_action_pressed("interact"):
-		SceneSwitcher.switch_scene("res://scenes/maps/daycare/playroom.tscn")
-'''
+
+func _ready():
+	Dialogic.start("timeline")
