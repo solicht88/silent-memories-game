14abab3a637d60ad990b405c2fb5f05e0cbfbf73
solicht88 <sakanaasai@gmail.com>
2024-07-24 17:21:43 -0400

    finished setting up bathroom, started staff_room
    - finished bathroom tileset, y-sort and collisions
    - started setting up staff_room tilesets

---
+++ scripts/scenes/daycare/doors/bathroom_door.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+
+func _on_interact():
+	SceneSwitcher.switch_scene("res://scenes/maps/daycare/bathroom.tscn")
