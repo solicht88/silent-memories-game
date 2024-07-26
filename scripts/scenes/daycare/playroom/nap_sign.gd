e465a53724853ed054931bf332340021d30f0a87
solicht88 <sakanaasai@gmail.com>
2024-07-25 22:31:41 -0400

    added interaction dialogue to playroom

---
+++ scripts/scenes/daycare/playroom/nap_sign.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var player = $"../player"
+
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+
+func _on_interact():
+	player.can_move = false
+	InteractionManager.can_interact = false
+	Dialogic.start("nap_sign")
+	Dialogic.timeline_ended.connect(_on_timeline_ended)
+
+func _on_timeline_ended():
+	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
+	player.can_move = true
+	InteractionManager.can_interact = true
