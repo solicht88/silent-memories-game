810588e0914c02586c0538730d62eeb667a4a2dc
solicht88 <sakanaasai@gmail.com>
2024-07-26 16:18:57 -0400

    Created and added scene_transition
    - May have small glitch when entering scene and playing fade_in animation

--- scripts/scenes/daycare/playroom/playroom.gd
+++ scripts/scenes/daycare/playroom/playroom.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
+@onready var transition = $Transition
+@onready var player = $player
+
 var reception = Vector2(200, 200)
 var calm_room = Vector2(1220, 480)
 var nap_room = Vector2(50, 480)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#transition.get_node("ColorRect").color.a = 255
+	transition.play("fade_out")
+	await get_tree().create_timer(1).timeout
+	player.can_move = true
+	InteractionManager.can_interact = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
--- scripts/scenes/daycare/reception/reception.gd
+++ scripts/scenes/daycare/reception/reception.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
+@onready var transition = $Transition
+@onready var player = $player
+
 var playroom = Vector2(640, 630)
 var bathroom = Vector2(640, 550)
 var staff_room = Vector2(1060, 400)
 var kitchen = Vector2(290, 480)
 
 func _ready():
-	pass
+	transition.play("fade_out")
+	await get_tree().create_timer(1).timeout
+	player.can_move = true
+	InteractionManager.can_interact = true
 	#Dialogic.signal_event.connect(_on_dialogic_signal)
 	#Dialogic.start("timeline")
 
